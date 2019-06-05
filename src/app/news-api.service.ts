49cef558f347f9a762db91ed6e8341f9a2fd92df
nader.hatami <nader.hatami@slalom.com>
2019-06-05 16:32:51 -0400

    Added NewsApi service to call news apis

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  api_key = '73e1750911db453f9077c789bf6b9010';
+
+  constructor(private http: HttpClient) {}
+
+  initSources() {
+    return this.http.get(
+      'https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key
+    );
+  }
+
+  initArticle() {
+    return this.http.get(
+      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' +
+        this.api_key
+    );
+  }
+
+  getArticleByID(source: String) {
+    return this.http.get(
+      'https://newsapi.org/v2/top-headlines?sources=' +
+        source +
+        '&apiKey=' +
+        this.api_key
+    );
+  }
+}
